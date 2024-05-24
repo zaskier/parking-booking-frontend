51c321e5581ab535dd73f2dd0c21c3e662c7400f
zaskier <zaskier@gmail.com>
2024-05-24 21:44:55 +0200

    #2 add icons, add redirects

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,14 +3,25 @@ import Link from "next/link";
 import { useRef, useState } from "react";
 import Image from "next/image";
 import menu from "../../../public/menu.svg";
+import reservationsIcon from "../../../public/reservation-icon.svg";
+import yourOffersIcon from "../../../public/offers-icon.svg";
+import accountIcon from "../../../public/account-icon.svg";
+import signInIconIcon from "../../../public/sign-in-icon.svg";
 
 export default function Navbar({}) {
   const [open, setOpen] = useState(false);
-  const menuItems = [
-    "Reservations",
-    "Your Parking Offers",
-    "Account",
-    "Sign In",
+
+  interface IMenuItem {
+    name: string;
+    redirect: string;
+    icon: string;
+  }
+
+  const menuItems: IMenuItem[] = [
+    { name: "Reservations", redirect: "/", icon: reservationsIcon }, //todo add proper redirects
+    { name: "Renting", redirect: "/", icon: yourOffersIcon },
+    { name: "Account", redirect: "/", icon: accountIcon },
+    { name: "Sign In", redirect: "/", icon: signInIconIcon },
   ];
   const menuRef = useRef();
   const imgRef = useRef();
@@ -30,7 +41,7 @@ export default function Navbar({}) {
         </Link>
         <div className="relative">
           <Image
-            className="w-10 h-30 flex pt-15"
+            className="w-10 h-30 flex pt-15 "
             ref={imgRef}
             src={menu}
             alt="menu"
@@ -46,10 +57,22 @@ export default function Navbar({}) {
                 {menuItems.map((element) => (
                   <li
                     onClick={() => setOpen(false)}
-                    className="p-1 text-lg cursor-pointer rounded hover:bg-purple-900"
+                    className="p-1 text-lg cursor-pointer rounded hover:bg-purple-900 sm:min-w-800 md:min-w-620"
                     key={element}
                   >
-                    {element}{" "}
+                    <span className="flex items-center justify-between">
+                      <Link href={element.redirect}>
+                        <Image
+                          className=""
+                          src={element.icon}
+                          alt="menu"
+                          onClick={() => setOpen((open) => !open)}
+                        ></Image>
+                        <div className="inline text-xl  text-right ">
+                          <span>{element.name} </span>
+                        </div>
+                      </Link>
+                    </span>
                   </li>
                 ))}
               </ul>
