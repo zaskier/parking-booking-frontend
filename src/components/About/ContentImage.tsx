173470f908a8eb3efbe881ff8f5e3ea5e88d9bae
zaskier <zaskier@gmail.com>
2024-06-24 11:54:13 +0200

    #6 add content image component, impove responsive design

---
+++ src/components/About/ContentImage.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import React from 'react';
+import savings from '../../../public/homepage/savings.svg';
+import location from '../../../public/homepage/location.svg';
+
+function ContentImageComponent() {
+  return (
+    <div>
+      <section className="grid gap-8 pl-16 pr-16 lg:grid-cols-2">
+        <Image className="sm::max-w-48 w-full p-20 md:max-w-96" src={savings} alt="savings"></Image>
+        <div className="w-1/2 sm:w-full">
+          <h2 className="pb-8 pt-8 text-2xl font-bold">
+            Unlock a new source of <span className="text-main-blue">passive income!</span>
+          </h2>
+          <p>
+            If you're heading on vacation or simply have an unused parking spot, why not let it work
+            for you? List your parking space on our app, and watch as other users conveniently rent
+            it during your downtime. Join our community of savvy parkers and turn your unused space
+            into extra cash – it's the easiest way to make your parking spot work for you!
+          </p>
+        </div>
+      </section>
+
+      <section className="grid gap-8 pl-16 pr-16 lg:grid-cols-2">
+        <div className="w-1/2 sm:w-full">
+          <h2 className="pb-8 pt-8 text-2xl font-bold">
+            <span className="text-main-blue">Save money</span> with our mobile app!{' '}
+          </h2>
+          <p>
+            We're on a mission to redefine both short-term and long-term parking by introducing
+            easily bookable spaces in bustling city centres and near major airports. Whether you
+            need a spot for a few hours or a permanent solution, we connect users seamlessly, making
+            parking stress a thing of the past. Join us in transforming urban mobility – because
+            finding the perfect parking space should be as easy as a tap away!
+          </p>
+        </div>
+        <div className="sm::max-w-48 w-full p-20 sm:max-w-96">
+          <Image src={location} alt="location"></Image>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default ContentImageComponent;
