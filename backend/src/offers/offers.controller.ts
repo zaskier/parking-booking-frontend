5719fd76a75b9c230d663bd44b9b34c46f183ed0
wojciech-iskierka <wojciech.iskierka@ingka.com>
2026-01-13 13:40:34 +0100

    #24 chore: limit upload file size, accept only images

--- backend/src/offers/offers.controller.ts
+++ backend/src/offers/offers.controller.ts
@@ -6,15 +6,18 @@ import {
   Query,
   UploadedFile,
   UseInterceptors,
-} from '@nestjs/common'
-import { CommandBus, QueryBus } from '@nestjs/cqrs'
-import { FileInterceptor } from '@nestjs/platform-express'
-import type { Express } from 'express'
-import { CreateOfferCommand } from './commands/impl/create-offer.command'
-import { CreateOfferDto } from './dtos/create-offer.dto'
-import { OfferType } from './enums/type.enum'
-import { FindAllOffersQuery } from './queries/impl/find-all-offers.query'
-import { UploadsService } from '../uploads/uploads.service'
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+} from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import type { Express } from 'express';
+import { CreateOfferCommand } from './commands/impl/create-offer.command';
+import { CreateOfferDto } from './dtos/create-offer.dto';
+import { OfferType } from './enums/type.enum';
+import { FindAllOffersQuery } from './queries/impl/find-all-offers.query';
+import { UploadsService } from '../uploads/uploads.service';
 
 @Controller('offers')
 export class OffersController {
@@ -26,7 +29,7 @@ export class OffersController {
 
   @Post()
   create(@Body() createOfferDto: CreateOfferDto) {
-    return this.commandBus.execute(new CreateOfferCommand(createOfferDto))
+    return this.commandBus.execute(new CreateOfferCommand(createOfferDto));
   }
 
   @Get()
@@ -35,16 +38,26 @@ export class OffersController {
     @Query('lat') lat?: number,
     @Query('lng') lng?: number,
   ) {
-    return this.queryBus.execute(new FindAllOffersQuery(type, lat, lng))
+    return this.queryBus.execute(new FindAllOffersQuery(type, lat, lng));
   }
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const uploadedFileUrl = await this.uploadsService.uploadFile(file)
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }), // 5MB
+          new FileTypeValidator({ fileType: /image\/(jpeg|png|gif)/ }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    const uploadedFileUrl = await this.uploadsService.uploadFile(file);
     return {
       message: 'File uploaded successfully',
       url: uploadedFileUrl,
-    }
+    };
   }
 }
--- backend/src/uploads/uploads.service.ts
+++ backend/src/uploads/uploads.service.ts
@@ -1,47 +1,51 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { S3Client, PutObjectCommand, S3ClientConfig } from '@aws-sdk/client-s3';
-import { v4 as uuid } from 'uuid';
-import type { Express } from 'express';
+import { Injectable, Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { S3Client, PutObjectCommand, S3ClientConfig } from '@aws-sdk/client-s3'
+import { v4 as uuid } from 'uuid'
+import type { Express } from 'express'
 
 @Injectable()
 export class UploadsService {
-  private readonly s3Client: S3Client;
+  private readonly s3Client: S3Client
+  private readonly logger = new Logger(UploadsService.name)
 
   constructor(private readonly configService: ConfigService) {
-    const region = this.configService.get<string>('AWS_REGION');
-    const accessKeyId = this.configService.get<string>('AWS_ACCESS_KEY_ID');
-    const secretAccessKey = this.configService.get<string>('AWS_SECRET_ACCESS_KEY');
+    const region = this.configService.get<string>('AWS_REGION')
+    const accessKeyId = this.configService.get<string>('AWS_ACCESS_KEY_ID')
+    const secretAccessKey = this.configService.get<string>(
+      'AWS_SECRET_ACCESS_KEY',
+    )
 
     const s3Config: S3ClientConfig = {
       region,
-    };
+    }
 
     if (accessKeyId && secretAccessKey) {
       s3Config.credentials = {
         accessKeyId,
         secretAccessKey,
-      };
+      }
     }
 
-    this.s3Client = new S3Client(s3Config);
+    this.s3Client = new S3Client(s3Config)
   }
 
   async uploadFile(file: Express.Multer.File): Promise<string> {
-    const bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
-    const fileName = `${uuid()}-${file.originalname}`;
+    this.logger.log(`File ${file.originalname} is uploaded`)
+    const bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME')
+    const fileName = `${uuid()}-${file.originalname}`
 
     const command = new PutObjectCommand({
       Bucket: bucketName,
       Key: fileName,
       Body: file.buffer,
       ContentType: file.mimetype,
-    });
+    })
 
-    await this.s3Client.send(command);
-
+    await this.s3Client.send(command)
+    this.logger.log(`file ${file.originalname} was sucesfully uploaded`)
     // Note: The public URL format can vary based on region and bucket settings.
     // This format is common but might need adjustment.
-    return `https://${bucketName}.s3.${this.configService.get<string>('AWS_REGION')}.amazonaws.com/${fileName}`;
+    return `https://${bucketName}.s3.${this.configService.get<string>('AWS_REGION')}.amazonaws.com/${fileName}`
   }
 }
